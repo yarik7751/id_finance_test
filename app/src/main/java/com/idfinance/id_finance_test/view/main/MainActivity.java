0c0d1e88f556d2159996f03847bc73336673b73a
yarik7751 <yaroslav.levshunov2@gmail.com>
2018-07-12 20:32:42 +0300

    first commit
    view elements are added

---
+++ app/src/main/java/com/idfinance/id_finance_test/view/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.idfinance.id_finance_test.view.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.idfinance.id_finance_test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
